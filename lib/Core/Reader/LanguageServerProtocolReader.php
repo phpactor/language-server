9b320fe1150cb32f22405d8a6b737a93cf4ca3c9
Daniel Leech <dan.t.leech@gmail.com>
2018-09-29 08:56:03 +0100

    rearranged reader

--- lib/Core/Reader/LanguageServerProtocolReader.php
+++ lib/Core/Reader/LanguageServerProtocolReader.php
@@ -11,6 +11,8 @@ use Psr\Log\LoggerInterface;
 
 class LanguageServerProtocolReader implements Reader
 {
+    const HEADER_CONTENT_LENGTH = 'Content-Length';
+
     /**
      * @var LoggerInterface
      */
@@ -23,47 +25,8 @@ class LanguageServerProtocolReader implements Reader
 
     public function readRequest(IO $io): Request
     {
-        $rawHeaders = [];
-        $buffer = [];
-        
-        while (true) {
-            $chunk = $io->read(1);
-
-            if (!$chunk->hasContents()) {
-                throw new ResetConnection('Input did not return anything');
-            }
-
-            $buffer[] = $chunk->contents();
-
-            if (count($buffer) >= 2 && array_slice($buffer, -2, 2) == [ "\r", "\n" ]) {
-                $header = trim(implode('', array_slice($buffer, 0, -2)));
-    
-                if (!$header) {
-                    break;
-                }
-    
-                $buffer = [];
-                $rawHeaders[] = $header;
-            }
-        }
-
-        $headers = $this->parseHeaders($rawHeaders);
-        $this->logger->debug('headers', $headers);
-
-        if (!array_key_exists('Content-Length', $headers)) {
-            throw new ServerError(sprintf(
-                'No valid Content-Length header provided in raw headers: "%s"',
-                implode(', ', $rawHeaders)
-            ));
-        }
-
-        $length = (int) $headers['Content-Length'];
-        if ($length < 1) {
-            throw new ServerError(sprintf(
-                'Content length must be greater than 0, got: %s',
-                $length
-            ));
-        }
+        $headers = $this->readHeaders($io);
+        $length = $this->getLengthFromHeaders($headers);
 
         $body = $io->read($length);
 
@@ -88,5 +51,57 @@ class LanguageServerProtocolReader implements Reader
         }
 
         return $parsed;
+    }
+
+    private function readHeaders(IO $io)
+    {
+            $rawHeaders = [];
+            $buffer = [];
+            
+            while (true) {
+                $chunk = $io->read(1);
+        
+                if (!$chunk->hasContents()) {
+                    throw new ResetConnection('Input did not return anything');
+                }
+        
+                $buffer[] = $chunk->contents();
+        
+                if (count($buffer) >= 2 && array_slice($buffer, -2, 2) == [ "\r", "\n" ]) {
+                    $header = trim(implode('', array_slice($buffer, 0, -2)));
+        
+                    if (!$header) {
+                        break;
+                    }
+        
+                    $buffer = [];
+                    $rawHeaders[] = $header;
+                }
+            }
+        
+            $headers = $this->parseHeaders($rawHeaders);
+        
+            $this->logger->debug('headers', $headers);
+        return $headers;
+    }
+
+    private function getLengthFromHeaders($headers)
+    {
+        if (!array_key_exists(self::HEADER_CONTENT_LENGTH, $headers)) {
+            throw new ServerError(sprintf(
+                'No valid Content-Length header provided in raw headers: "%s"',
+                implode(', ', array_keys($headers))
+            ));
+        }
+        
+        $length = (int) $headers[self::HEADER_CONTENT_LENGTH];
+        
+        if ($length < 1) {
+            throw new ServerError(sprintf(
+                'Content length must be greater than 0, got: %s',
+                $length
+            ));
+        }
+        return $length;
     }
 }
