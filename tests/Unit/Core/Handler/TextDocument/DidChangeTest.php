9b14c92dfa251bc52078f0e4b0ab991e344167f2
Daniel Leech <dan.t.leech@gmail.com>
2018-09-29 13:38:30 +0100

    adds test

--- tests/Unit/Core/Handler/TextDocument/DidChangeTest.php
+++ tests/Unit/Core/Handler/TextDocument/DidChangeTest.php
@@ -2,12 +2,15 @@
 
 namespace Phpactor\LanguageServer\Tests\Unit\Core\Handler\TextDocument;
 
+use LanguageServerProtocol\ContentChangeEvent;
 use LanguageServerProtocol\TextDocumentContentChangeEvent;
 use LanguageServerProtocol\TextDocumentItem;
 use LanguageServerProtocol\VersionedTextDocumentIdentifier;
 use Phpactor\LanguageServer\Core\Handler;
+use Phpactor\LanguageServer\Core\Handler\TextDocument\DidChange;
 use Phpactor\LanguageServer\Core\Handler\TextDocument\DidOpen;
 use Phpactor\LanguageServer\Tests\Unit\Core\Handler\HandlerTestCase;
+use phpDocumentor\Reflection\DocBlock\Tags\Version;
 
 class DidChangeTest extends HandlerTestCase
 {
@@ -18,5 +21,18 @@ class DidChangeTest extends HandlerTestCase
 
     public function testUpdatesDocument()
     {
+        $document = new TextDocumentItem();
+        $document->uri = 'foobar';
+        $this->session()->current()->workspace()->open($document);
+        $this->dispatch('textDocument/didChange', [
+            'textDocument' => new VersionedTextDocumentIdentifier('foobar'),
+            'contentChanges' => [
+                [
+                    'text' => 'asd',
+                ]
+            ],
+        ]);
+
+        $this->assertEquals('asd', $document->text);
     }
 }
