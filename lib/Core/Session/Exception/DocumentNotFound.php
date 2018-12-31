dfe5dbcc7a75afd3e140c2c734e619a43461cc94
Daniel Leech <dan.t.leech@gmail.com>
2018-12-31 13:00:33 +0000

    Revert "load not registered documents"
    This reverts commit c229bba4c231c565700dab3daa1c730f0c9f126f.

--- lib/Core/Session/Exception/DocumentNotFound.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Phpactor\LanguageServer\Core\Session\Exception;
-
-use RuntimeException;
-
-class DocumentNotFound extends RuntimeException
-{
-}
--- tests/Unit/Core/Session/WorkspaceTest.php
+++ tests/Unit/Core/Session/WorkspaceTest.php
@@ -5,7 +5,6 @@ namespace Phpactor\LanguageServer\Tests\Unit\Core\Session;
 use LanguageServerProtocol\TextDocumentItem;
 use LanguageServerProtocol\VersionedTextDocumentIdentifier;
 use PHPUnit\Framework\TestCase;
-use Phpactor\LanguageServer\Core\Session\Exception\DocumentNotFound;
 use Phpactor\LanguageServer\Core\Session\Exception\UnknownDocument;
 use Phpactor\LanguageServer\Core\Session\Workspace;
 
@@ -23,7 +22,7 @@ class WorkspaceTest extends TestCase
 
     public function testThrowsExceptionGetUnknown()
     {
-        $this->expectException(DocumentNotFound::class);
+        $this->expectException(UnknownDocument::class);
         $this->workspace->get('foobar');
     }
 
@@ -35,14 +34,6 @@ class WorkspaceTest extends TestCase
         $document = $this->workspace->get('foobar');
 
         $this->assertSame($expectedDocument, $document);
-    }
-
-    public function testLoadsCurrentlyNotLoadedDocument()
-    {
-        $this->workspace->get(__FILE__);
-        $document = $this->workspace->get(__FILE__);
-
-        $this->assertEquals(file_get_contents(__FILE__), $document->text);
     }
 
     public function testThrowsExceptionUpdateUnknown()
