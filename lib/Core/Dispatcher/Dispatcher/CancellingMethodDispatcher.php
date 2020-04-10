1360bf2c24a5deb15e1b99021aa35db4b4025c23
Daniel Leech <dan.t.leech@gmail.com>
2020-04-10 10:05:07 +0100

    Added cancellation middleware

---
+++ lib/Core/Dispatcher/Dispatcher/CancellingMethodDispatcher.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Phpactor\LanguageServer\Core\Dispatcher\Dispatcher;
+
+use Amp\CancellationTokenSource;
+use Amp\CancelledException;
+use Amp\Promise;
+use Amp\Success;
+use LanguageServerProtocol\ErrorCode;
+use Phpactor\LanguageServer\Core\Dispatcher\Dispatcher;
+use Phpactor\LanguageServer\Core\Handler\Handlers;
+use Phpactor\LanguageServer\Core\Rpc\ErrorCodes;
+use Phpactor\LanguageServer\Core\Rpc\RequestMessage;
+use Phpactor\LanguageServer\Core\Rpc\ResponseError;
+use Phpactor\LanguageServer\Core\Rpc\ResponseMessage;
+use Psr\Log\LoggerInterface;
+
+class CancellingMethodDispatcher implements Dispatcher
+{
+    const METHOD_CANCEL_REQUEST = '$/cancelRequest';
+
+    /**
+     * @var Dispatcher
+     */
+    private $innerDispatcher;
+
+    /**
+     * @var array<int, CancellationTokenSource>
+     */
+    private $cancellations = [];
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(Dispatcher $innerDispatcher, LoggerInterface $logger)
+    {
+        $this->innerDispatcher = $innerDispatcher;
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function dispatch(
+        Handlers $handlers,
+        RequestMessage $request,
+        array $extraArgs
+    ): Promise
+    {
+        return \Amp\call(function () use ($handlers, $request, $extraArgs) {
+            if ($request->method === self::METHOD_CANCEL_REQUEST) {
+                return $this->cancelRequest($request);
+            }
+
+            $cancellationTokenSource = new CancellationTokenSource();
+
+            // we only cancel requests (that have IDs) and not notifications
+            if (is_int($request->id)) {
+                $this->cancellations[$request->id] = $cancellationTokenSource;
+            }
+
+            $extraArgs['_cancel'] = $cancellationTokenSource->getToken();
+
+            $response = yield $this->innerDispatcher->dispatch($handlers, $request, $extraArgs);
+
+            return $response;
+        });
+    }
+
+    private function cancelRequest(RequestMessage $request): void
+    {
+        $id = $request->params['id'];
+        if (!isset($this->cancellations[$id])) {
+            $this->logger->warning(sprintf(
+                'Trying to cancel non-running request "%s", running requests: "%s"',
+                $id,
+                implode('", "', array_keys($this->cancellations))
+            ));
+            return;
+        }
+
+        $tokenSource = $this->cancellations[$id];
+        assert($tokenSource instanceof CancellationTokenSource);
+        $tokenSource->cancel();
+    }
+
+    private function createCancellationMessage(int $id, string $message): ResponseMessage
+    {
+        return new ResponseMessage(
+            $id,
+            null,
+            new ResponseError(
+                ErrorCodes::RequestCancelled,
+                $message
+            )
+        );
+    }
+}
