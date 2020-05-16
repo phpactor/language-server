e924ea0bca5ff356f050cf88dab44a19bdcf86f2
Daniel Leech <dan.t.leech@gmail.com>
2020-05-16 08:06:47 +0100

    Adds command dispatcher

---
+++ lib/Workspace/CommandDispatcher.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Phpactor\LanguageServer\Workspace;
+
+use RuntimeException;
+
+class CommandDispatcher
+{
+    /**
+     * @var array
+     */
+    private $commandMap = [];
+
+    public function __construct(array $commandMap)
+    {
+        foreach ($commandMap as $id => $command) {
+            $this->addCommand($id, $command);
+        }
+    }
+
+    private function addCommand(string $id, object $invokable): void
+    {
+        if (!is_callable($invokable)) {
+            throw new RuntimeException(sprintf(
+                'Object "%s" is not invokable', get_class($invokable)
+            ));
+        }
+
+        $this->commandMap[$id] = $invokable;
+    }
+
+    /**
+     * @param array<int,mixed> $args
+     *
+     * @return mixed
+     */
+    public function dispatch(string $command, array $args = [])
+    {
+        if (!isset($this->commandMap[$command])) {
+            throw new RuntimeException(sprintf(
+                'Command "%s" not found, known commands: "%s"',
+                $command, implode('", "', array_keys($this->commandMap))
+            ));
+        }
+
+        return $this->commandMap[$command]->__invoke(...$args);
+    }
+}
---
+++ tests/Unit/Handler/Workspace/CommandHandlerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Phpactor\LanguageServer\Tests\Unit\Handler\Workspace;
+
+use Phpactor\LanguageServer\Core\Handler\Handler;
+use Phpactor\LanguageServer\Core\Server\Exception\ExitSession;
+use Phpactor\LanguageServer\Handler\System\ExitHandler;
+use Phpactor\LanguageServer\Handler\Workspace\CommandHandler;
+use Phpactor\LanguageServer\Tests\Unit\Handler\HandlerTestCase;
+use Phpactor\LanguageServer\Workspace\CommandDispatcher;
+
+class CommandHandlerTest extends HandlerTestCase
+{
+    public function handler(): Handler
+    {
+        return new CommandHandler(new CommandDispatcher([
+            'foobar' => new class {
+                public function __invoke(string $arg) {
+                    return $arg;
+                }
+            }
+        ]));
+    }
+
+    public function testExecutesCommand()
+    {
+        $result = $this->dispatch('workspace/executeCommand', [
+            'command' => 'foobar',
+            'arguments' => [
+                'barfoo',
+            ],
+        ]);
+
+
+    }
+}
