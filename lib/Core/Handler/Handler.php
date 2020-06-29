03d074d5374e0423b26196493f6e62c9bcaf3fec
Daniel Leech <dan.t.leech@gmail.com>
2020-06-29 20:09:26 +0100

    Add documentation

--- lib/Core/Handler/Handler.php
+++ lib/Core/Handler/Handler.php
@@ -2,6 +2,11 @@
 
 namespace Phpactor\LanguageServer\Core\Handler;
 
+/**
+ * Language Server Handler
+ *
+ * See documentation for the `methods` method for more details.
+ */
 interface Handler
 {
     /**
@@ -17,11 +22,30 @@ interface Handler
      *         ];
      *     }
      *
-     *     public function didOpen(): Generator
+     *     public function didOpen(TextDocumentItem $item): Promise
      *     {
      *         yield new SomeRespose();
      *     }
      * ```
+     *
+     * Each method MUST return an `Amp\Promise`.
+     *
+     * The arguments passed by the RPC call are automatically resolved.
+     *
+     * LSP params
+     * ----------
+     *
+     * If you implement the LSP `*Params` object, e.g. `initialize(InitializeParams $params)`
+     * then the RPC arguments will be coerced into a `Phpactor\LanguageServerProtocol\InitializeParams` object.
+     *
+     * You can also have a second parameter with hte `Amp\CancellationToken` which will
+     * allow you to cancel your subrouting.
+     *
+     * RPC params
+     * ----------
+     *
+     * If no LSP parameter type is defined, then the arguments will be mapped directly to
+     * your parameters.
      */
     public function methods(): array;
 }
