e0c099555355f9bfcddcd4490237ebf806d05b9d
Daniel Leech <dan.t.leech@gmail.com>
2020-08-20 11:05:00 +0100

    Add logger to aggregate diagnostics provider

--- lib/Core/Diagnostics/AggregateDiagnosticsProvider.php
+++ lib/Core/Diagnostics/AggregateDiagnosticsProvider.php
@@ -4,6 +4,8 @@ namespace Phpactor\LanguageServer\Core\Diagnostics;
 
 use Amp\Promise;
 use Phpactor\LanguageServerProtocol\TextDocumentItem;
+use Psr\Log\LoggerInterface;
+use Throwable;
 use function Amp\call;
 
 class AggregateDiagnosticsProvider implements DiagnosticsProvider
@@ -13,9 +15,15 @@ class AggregateDiagnosticsProvider implements DiagnosticsProvider
      */
     private $providers;
 
-    public function __construct(DiagnosticsProvider ...$providers)
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger, DiagnosticsProvider ...$providers)
     {
         $this->providers = $providers;
+        $this->logger = $logger;
     }
 
     /**
@@ -26,10 +34,26 @@ class AggregateDiagnosticsProvider implements DiagnosticsProvider
         return call(function () use ($textDocument) {
             $diagnostics = [];
             foreach ($this->providers as $provider) {
-                $diagnostics = array_merge(
-                    $diagnostics,
-                    yield $provider->provideDiagnostics($textDocument)
-                );
+                try {
+                    $start = microtime(true);
+                    $diagnostics = array_merge(
+                        $diagnostics,
+                        yield $provider->provideDiagnostics($textDocument)
+                    );
+                    $this->logger->debug(sprintf(
+                        'Diagnostic finsihed in "%s" (%s)',
+                        number_format(microtime(true) - $start, 2),
+                        get_class($provider)
+                    ));
+                } catch (\Throwable $throwable) {
+                    $this->logger->error(sprintf(
+                        'Diagnostic error from provider "%s": %s',
+                        get_class($provider),
+                        $throwable->getMessage()
+                    ), [
+                        'trace' => $throwable->getTraceAsString()
+                    ]);
+                }
             }
 
             return $diagnostics;
