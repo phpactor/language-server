a4fb3321241838bbe0d037a67243feb668afb05a
Daniel Leech <dan.t.leech@gmail.com>
2021-04-24 11:03:29 +0100

    Add collection methods to files changed

--- lib/Event/FilesChanged.php
+++ lib/Event/FilesChanged.php
@@ -2,7 +2,10 @@
 
 namespace Phpactor\LanguageServer\Event;
 
+use Phpactor\LanguageServerProtocol\FileChangeType;
 use Phpactor\LanguageServerProtocol\FileEvent;
+use RuntimeException;
+use function array_shift;
 
 final class FilesChanged
 {
@@ -19,5 +22,25 @@ final class FilesChanged
     public function events(): array
     {
         return $this->events;
+    }
+
+    public function byType(int $type): self
+    {
+        return new self(...array_filter($this->events, function (FileEvent $event) use ($type) {
+            return $event->type === $type;
+        }));
+    }
+
+    public function first(): FileEvent
+    {
+        $first = reset($this->events);
+
+        if (null === $first) {
+            throw new RuntimeException(
+                'No file events, cannot get the first one',
+            );
+        }
+
+        return $first;
     }
 }
