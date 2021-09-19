ed60d0f6a511acd1754457d53cbc4f9245c04ee4
Daniel Leech <dan.t.leech@gmail.com>
2021-09-19 18:31:33 +0100

    Adde ClosureCommand class for testing

---
+++ lib/Core/Command/ClosureCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Phpactor\LanguageServer\Core\Command;
+
+use Closure;
+
+class ClosureCommand implements Command
+{
+    /**
+     * @var Closure
+     */
+    private $closure;
+
+    public function __construct(Closure $closure)
+    {
+        $this->closure = $closure;
+    }
+
+    /**
+     * @param mixed[] $args
+     * @return mixed
+     */
+    public function __invoke(...$args) {
+        $closure = $this->closure;
+        return $closure(...$args);
+    }
+}
