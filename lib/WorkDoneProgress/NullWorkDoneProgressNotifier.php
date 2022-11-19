4cf9e8e3765ac8c6a26ad11da53ec966095fd0b1
Daniel Leech <dan.t.leech@gmail.com>
2022-11-19 11:35:30 +0100

    ADd disabled workdonw progress notifier

---
+++ lib/WorkDoneProgress/NullWorkDoneProgressNotifier.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Phpactor\LanguageServer\WorkDoneProgress;
+
+use Amp\Promise;
+use Amp\Success;
+use Ramsey\Uuid\Uuid;
+use Phpactor\LanguageServer\Core\Rpc\ResponseMessage;
+
+class NullWorkDoneProgressNotifier implements ProgressNotifier
+{
+    public function create(WorkDoneToken $token): Promise
+    {
+        return new Success(new ResponseMessage(
+            Uuid::uuid4(),
+            null,
+        ));
+    }
+
+    public function begin(WorkDoneToken $token, string $title, ?string $message = null, ?int $percentage = null, ?bool $cancellable = null): void
+    {
+    }
+
+    public function report(WorkDoneToken $token, ?string $message = null, ?int $percentage = null, ?bool $cancellable = null): void
+    {
+    }
+
+    public function end(WorkDoneToken $token, ?string $message = null): void
+    {
+    }
+}
